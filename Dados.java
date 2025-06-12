8017bc0844292f8e3d19a52f27bdf768471e5f10
Samires do Carmo <samiresdocarmo@gmail.com>
2025-06-12 07:45:50 -0300

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -1,93 +1,87 @@
 package br.com.maisunifacisa;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Dados {
-	private List<Tarefa> atividades = new ArrayList<>();
-	private List<Usuario> usuarios = new ArrayList<>();
+    private List<Tarefa> atividades = new ArrayList<>();
+    private List<Usuario> usuarios = new ArrayList<>();
 
-	public List<Usuario> getUsuarios() {
-		return usuarios;
-	}
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
 
-	public void setUsuarios(List<Usuario> usuarios) {
-		this.usuarios = usuarios;
-	}
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
 
-	public List<Tarefa> getAtividades() {
-		return atividades;
-	}
+    public List<Tarefa> getAtividades() {
+        return atividades;
+    }
 
-	public void setAtividades(List<Tarefa> atividades) {
-		this.atividades = atividades;
-	}
-	public void adicionarAtividade(Tarefa atividade) {
-		atividades.add(atividade);
-	}
-	public void adicionarUsuario(Usuario usuario) {
-		usuarios.add(usuario);
+    public void setAtividades(List<Tarefa> atividades) {
+        this.atividades = atividades;
+    }
 
-	}
-	public Boolean excluirTarefa(String titulo) {
-		for(Tarefa tarefa : atividades) {
-			if(tarefa.getTitulo().equals(titulo)) {
-				atividades.remove(tarefa);
-				return true;
-			}
-		}
-		return false;
-	}
+    public void adicionarAtividade(Tarefa atividade) {
+        atividades.add(atividade);
+    }
 
-	public boolean iniciarTarefa(String titulo) {
-		for(Tarefa tarefa: atividades) {
-			if(tarefa.getTitulo().equalsIgnoreCase(titulo)) {
-				tarefa.setStatusTarefa(Status.EM_ANDAMENTO);
-				return true;
-			}
-		}
-		return false;
-	}
+    public void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
 
-	public boolean finalizarTarefa(String titulo) {
-		for(Tarefa tarefa: atividades) {
-			if(tarefa.getTitulo().equalsIgnoreCase(titulo)) {
-				tarefa.setStatusTarefa(Status.CONCLUIDA);
-				return true;
-			}
-		}
-		return false;
-		}
-	
-	public void exibirAtividades() {
-		if (atividades.isEmpty()) {
-			System.out.println("Nenhuma atividade cadastrada.");
-		} else {
-			for (Tarefa atividade : atividades) {
-				atividade.exibirDetalhes();
-			}
-		}
-		}
-	public void buscarPorUsuario(String nomeUsuario) {
-		boolean encontrado = false;
-		for (Tarefa atividade : atividades) {
-			if (atividade.getUsuarioResponsavel().getNome().equalsIgnoreCase(nomeUsuario)) {
-				atividade.exibirDetalhes();
-				encontrado = true;
-			}
-		}
-		if (!encontrado) {
-			System.out.println("Nenhuma atividade encontrada para o usuário: " + nomeUsuario);
-		}
-	}
-	public Boolean buscarPeloTitulo(String titulo) {
-		for(Tarefa tarefa : atividades) {
-			if(tarefa.getTitulo().equals(titulo)) {
-				return true;
-				}
-			}
-				return false;
-		}
+    public Boolean excluirTarefa(String titulo) {
+        for (Tarefa tarefa : atividades) {
+            if (tarefa.getTitulo().equals(titulo)) {
+                atividades.remove(tarefa);
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public boolean iniciarTarefa(String titulo) {
+        for (Tarefa tarefa : atividades) {
+            if (tarefa.getTitulo().equalsIgnoreCase(titulo)) {
+                tarefa.setStatus(StatusTarefa.EM_ANDAMENTO);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean finalizarTarefa(String titulo) {
+        for (Tarefa tarefa : atividades) {
+            if (tarefa.getTitulo().equalsIgnoreCase(titulo)) {
+                tarefa.setStatus(StatusTarefa.CONCLUIDA);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void buscarPorUsuario(String nomeUsuario) {
+        boolean encontrado = false;
+        for (Tarefa atividade : atividades) {
+            if (atividade.getUsuarioResponsavel().getNome().equalsIgnoreCase(nomeUsuario)) {
+                atividade.exibirDetalhes();
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println(
+                "Nenhuma atividade encontrada para o usuário: " + nomeUsuario
+            );
+        }
+    }
+
+    public Boolean buscarPeloTitulo(String titulo) {
+        for (Tarefa tarefa : atividades) {
+            if (tarefa.getTitulo().equals(titulo)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
